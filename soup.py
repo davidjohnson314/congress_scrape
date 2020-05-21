7c892cfc3a1b68b5dc46f69d5044c8f2dbeab9bd
DJ <davidjohnson314@gmail.com>
2020-05-20 21:57:34 -0400

    did shit

--- soup.py
+++ soup.py
@@ -3,60 +3,75 @@ import csv
 from bs4 import BeautifulSoup
 from csv import writer
 
-response = requests.get(
-#'https://www.congress.gov/bill/115th-congress/senate-bill/2760?q=%7B%22search%22%3A%22air+pollution%22%7D&s=1&r=1'
-'https://www.congress.gov/bill/115th-congress/senate-joint-resolution/21?q=%7B%22search%22%3A%22air+pollution%22%7D&s=1&r=2'
-#'https://www.congress.gov/quick-search/legislation?wordsPhrases=air+pollution&wordVariants=on&congresses%5B%5D=115&congresses%5B%5D=114&congresses%5B%5D=113&congresses%5B%5D=112&congresses%5B%5D=111&legislationNumbers=&legislativeAction=&sponsor=on&representative=&senator=&searchResultViewType=expanded&KWICView=false'
-)
-response2 = requests.get(
-'https://www.congress.gov/bill/115th-congress/senate-bill/987/text?format=txt&q=%7B%22search%22%3A%22air+pollution%22%7D&r=59&s=2'
-)
+#bill scraping loop
+def billScrape(soup,writer):
+        #sponsor = soup.find_all("table", class_="standard01")
+    for sponsor in soup.find_all("table", class_="standard01"):
+        sponsor_text = sponsor.find("a", target="_blank").get_text()
+        #print(sponsor_text)
 
-soup = BeautifulSoup(response.text, 'html.parser')
-soup2 = BeautifulSoup(response2.text, 'html.parser')
-#soup_lxml = BeautifulSoup(response.text, 'lxml')
-#soup_html5 = BeautifulSoup(response.text, 'html5lib')
+    #title = soup.find_all("h1", class_="legDetail")    
+    for title in soup.find_all("h1", class_="legDetail"):
+        title_text = title.get_text()
+        #print(title_text)
 
-everything = soup.find_all('html')
-   
+    #name = soup.find_all("h2", class_="primary")
+    for name in soup.find_all("h2", class_="primary"):
+        name_text = name.get_text()
+        #print(name_text)
+
+
+    #for tracker in soup.find_all("li", class_="first selected last"):
+        #tracker_text = tracker.get_text()
+        #print(tracker_text)
+
+    #tracker2 = soup.find_all("h3", class_="currentVersion")    
+    for tracker2 in soup.find_all("h3", class_="currentVersion"):
+        tracker2_text = tracker2.find("span").get_text()
+        # print(tracker2_text)
+        
+    writer.writerow([sponsor_text,title_text,name_text,tracker2_text])
+
+#csv writer
 file = open('bills.csv', 'a')
 writer = csv.writer(file)
-
 writer.writerow(['Sponsor', 'Title', 'Name', 'Tracker'])
-   
 
-for bill_search in soup.find_all("span", class_="result-heading"):
-    bill_search_link = bill_search.find("a", href=True)
-    #print(bill_search_link['href'])
+#link search
+userSearchTerms = input("type search terms:")
+userSearchFormatted = userSearchTerms.replace(" ", "+")
+queryString = 'https://www.congress.gov/quick-search/legislation?wordsPhrases=' + userSearchFormatted + '&wordVariants=on&congresses%5B%5D=115&congresses%5B%5D=114&congresses%5B%5D=113&congresses%5B%5D=112&congresses%5B%5D=111&legislationNumbers=&legislativeAction=&sponsor=on&representative=&senator=&searchResultViewType=expanded&KWICView=false'
+#print(queryString)
+searchResults = requests.get(queryString)
+
+soupSearch = BeautifulSoup(searchResults.text, 'html.parser')
+
+for bill_search in soupSearch.find_all("span", class_="result-heading"):
+    bill_search_links = bill_search.find("a", href=True)
+    print(bill_search_links['href'])
+
+for link in bill_search_links['href']:
+    print(link)
     
-#sponsor = soup.find_all("table", class_="standard01")
-for sponsor in soup.find_all("table", class_="standard01"):
-    sponsor_text = sponsor.find("a", target="_blank").get_text()
-    #print(sponsor_text)
+    billPage = requests.get(link)
+    soupBillPage = BeautifulSoup(billPage.text, 'html.parser')
 
-#title = soup.find_all("h1", class_="legDetail")    
-for title in soup.find_all("h1", class_="legDetail"):
-    title_text = title.get_text()
-    #print(title_text)
-
-#name = soup.find_all("h2", class_="primary")
-for name in soup.find_all("h2", class_="primary"):
-    name_text = name.get_text()
-    #print(name_text)
+    billScrape(soupBillPage,writer)
 
 
-#for tracker in soup.find_all("li", class_="first selected last"):
-    #tracker_text = tracker.get_text()
-    #print(tracker_text)
-
-#tracker2 = soup.find_all("h3", class_="currentVersion")    
-for tracker2 in soup.find_all("h3", class_="currentVersion"):
-    tracker2_text = tracker2.find("span").get_text()
-    print(tracker2_text)
-    
-writer.writerow([sponsor_text,title_text,name_text,tracker2_text])
-    
 file.close()
+
+
+
+
+
+# for line in file:
+    # if 'Sponsor' not in line:
+        # writer.writerow(['Sponsor', 'Title', 'Name', 'Tracker'])
+    # break
+
+# def add_to_csv(row):
+    # with open('output.csv', 'a') as f:
 
 
 #for bill in soup2.find('pre', id='billTextContainer'):
